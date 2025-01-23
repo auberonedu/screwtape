034d217b0d12f1c1ce07178218de6e32ddb7702b
Madeleine Chance <mechance782@gmail.com>
2025-01-22 18:49:26 -0800

    implemented Node constructor for List param

--- src/Node.java
+++ src/Node.java
@@ -38,7 +38,36 @@ public class Node {
    * @throws IllegalArgumentException If the list is null or empty.
    */
   public Node(List<Integer> list) {
-    // TODO: implement this
+    if (list == null || list.isEmpty()){
+      throw new IllegalArgumentException("List cannot be null or empty");
+    }
+
+    // if list has 1 element, set head value to that element (at index 0), 
+    // and leave prev and next fields null
+    this.value = list.get(0);
+
+    // if list has more than 1 element, create a new Node with the value at index 1,
+    // set it's prev to point at the constructed node and point this.next at it. 
+    if (list.size() > 1){
+      Node curr = new Node(list.get(1));
+      this.next = curr;
+      Node past = this;
+
+      // iterate through the rest of the list
+      for(int i = 2; i < list.size(); i++){
+        // set curr.next to point at a new node
+        curr.next = new Node(list.get(i));
+        // set curr.prev to point at the past node (starting at the head node)
+        curr.prev = past;
+        // increment curr and past nodes. curr now points at the new node
+        // and past now points at the node behind curr
+        past = past.next;
+        curr = curr.next;
+      }
+      // set prev node for the last node of the list
+      curr.prev = past;
+    }
+    
   }
 
   /**
